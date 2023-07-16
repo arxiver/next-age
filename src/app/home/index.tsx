edbe09de174e6b290d2edaaf5a431bcc9d6ce2b4
Mokhtar <rrrokhtar@gmail.com>
2023-07-17 00:45:47 +0300

    Refactored componenets

--- src/app/home/index.tsx
+++ src/app/home/index.tsx
@@ -1,13 +1,13 @@
-import { AGBanner } from '@/app/home/sections/Banner';
-import { AGBio } from '@/app/home/sections/Bio';
-import { AGFeatures } from '@/app/home/sections/Features';
-import { AGCodeview } from '@/app/home/sections/Codeview';
-import { AGPlans } from '@/app/home/sections/Plans';
-import { AGBenefits } from '@/app/home/sections/Benefits';
-import { AGOpenings } from '@/app/home/sections/Openings';
-import { AGContact } from '@/app/home/sections/Contact';
-import { AGFooter } from '@/app/home/sections/Footer';
-import { AGNavbar } from '@/app/home/sections/Navbar';
+import { AGBanner } from '@/app/home/sections/banner';
+import { AGBio } from '@/app/home/sections/bio';
+import { AGFeatures } from '@/app/home/sections/features';
+import { AGCodeview } from '@/app/home/sections/codeview';
+import { AGPlans } from '@/app/home/sections/plans';
+import { AGBenefits } from '@/app/home/sections/benefits';
+import { AGOpenings } from '@/app/home/sections/openings';
+import { AGContact } from '@/app/home/sections/contact';
+import { AGFooter } from '@/app/home/sections/footer';
+import { AGNavbar } from '@/app/home/sections/navbar';
 
 export default function Home() {
   return (
