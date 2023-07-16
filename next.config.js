da6133f370d540fa34fc836f74f85543ca70ea26
Mokhtar <rrrokhtar@gmail.com>
2023-07-16 21:09:25 +0300

    Revert "Revert "Revert "Updated config"""
    This reverts commit cd492f80a5c114ff8a0d6c46160aaadd733c99cc.

--- next.config.js
+++ next.config.js
@@ -6,6 +6,14 @@ const nextConfig = {
   // Optional: Change the output directory `out` -> `dist`
   // distDir: 'dist',
   reactStrictMode: true,
+<<<<<<< HEAD
+=======
+  images: {
+    loader: "akamai",
+    path: "/",
+    unoptimized: true
+  },
+>>>>>>> 3114dfd (Inital codebase page (#1))
   compiler:{
     styledComponents: true
   }
